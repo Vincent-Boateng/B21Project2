d2cf0944e5e9b104f22b5eca27419a15544cfd0a
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-10 16:18:58 -0500

    just created a new class with some updates

---
+++ src/LoginService.java
@@ -0,0 +1,9 @@
+public class LoginService {
+
+    public static void main(String[] args) {
+
+        //new file to help understand branching
+        //master and head
+
+    }
+}
---
+++ src/LoginUtility.java
@@ -0,0 +1,9 @@
+public class LoginUtility {
+
+    public static void main(String[] args) {
+
+        //the head always follows
+        //branching is fun
+
+    }
+}

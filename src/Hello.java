e6410f85e133e7be9a8656b952c5b79e59bd0b2f
Vincent-Boateng <vincentboateng@gmail.com>
2020-11-10 15:40:55 -0500

    learning about branch

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,9 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello");
+        //Learn about branching
+        //what is Java?
+        //File in progress
+
     }
 }
